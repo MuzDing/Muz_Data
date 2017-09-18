6d725faf13db2cce710e42548542bbb47719f7a7
MuzDing <ld56888@163.com>
2017-09-18 21:26:44 +0800

    remens

--- test2/test/6.6.py
+++
@@ -1,9 +0,0 @@
-class Person():
-    def __init__(self,name):
-        self.name = name
-
-class EmailPerson(Person):
-    def __init__(self,name,email):
-        super().__init__(name)
-        self.email=email
-
